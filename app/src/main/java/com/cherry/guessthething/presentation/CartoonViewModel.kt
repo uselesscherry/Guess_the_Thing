2924d6af994f4f05dedc57094fc87153a9a13440
serhii tyshchenko <serheytischenko666gmail.com>
2022-07-12 20:27:02 +0300

    cleared some imports

--- app/src/main/java/com/cherry/guessthething/presentation/CartoonViewModel.kt
+++ app/src/main/java/com/cherry/guessthething/presentation/CartoonViewModel.kt
@@ -1,13 +1,11 @@
 package com.cherry.guessthething.presentation
 
-import android.app.Application
 import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.cherry.guessthething.data.RepositoryImpl
 import com.cherry.guessthething.domain.Repository
 import com.cherry.guessthething.domain.model.Cartoon
 import kotlinx.coroutines.launch
